cc594eab4bbc71e124d41cfcf2a631f54536d22b
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2020-08-06 01:17:33 -0300

    Conteudo da terceira aula - consumindo e persistindo dados no bd

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
+export interface Teacher {
+    id: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
 
-function TeacherItem(){
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/44446985?s=400&u=b87972ae1cb8ffd94022e171bb637b9eb22801f6&v=4" alt="" />
+                <img src={teacher.avatar} alt="" />
                 <div>
-                    <strong>Guilherme Lima Soares</strong>
+                    <strong>{teacher.name}</strong>
+                    <strong>{teacher.subject}</strong>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnoloias de quimica avançada.
-                <br /><br/>
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 20,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
 
-                <button type="button">
+                <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}>
+                        
                     <img src={whatsappIcon} alt="Entrar em contato" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
